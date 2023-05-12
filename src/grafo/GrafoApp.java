c366565a160cbd808f630ae86872b4e06464217a
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2023-05-12 11:42:49 -0300

    Update GrafoApp.java

--- src/grafo/GrafoApp.java
+++ src/grafo/GrafoApp.java
@@ -3,13 +3,14 @@ package grafo;
 public class GrafoApp {
 
 	public static void main(String[] args) {
-		
+				
 		GrafoRepresentacao rep = new GrafoRepresentacao();
 		
 		rep.direcao();
 		rep.recebeVertices();
 		rep.criaArestas();
 		rep.mostraListaAdj();
+		rep.mostraMatrizAdj();
 	}
 
 }
