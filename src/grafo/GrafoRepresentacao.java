a7559c3b4da1ba86d1c0e3798ffbb47b7cf7e5b7
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2023-05-12 11:44:15 -0300

    Update GrafoRepresentacao.java

--- src/grafo/GrafoRepresentacao.java
+++ src/grafo/GrafoRepresentacao.java
@@ -1,10 +1,10 @@
 package grafo;
 /**********************************
  * Aluno: Fernando Rodrigo Pinheiro de Sousa
- * Descrição:
- * Representações de Grafos por meio de Listas e Matrizes de Adjacências;
- * retorna o número de vértices; 
- * retorna o número de arestas; 
+ * DescriÃ§Ã£o:
+ * RepresentaÃ§Ãµes de Grafos por meio de Listas e Matrizes de AdjacÃªncias;
+ * retorna o nÃºmero de vÃ©rtices; 
+ * retorna o nÃºmero de arestas; 
  * Adiciona arestas;
  * retorna arestas adjacentes.
  **********************************/
@@ -15,7 +15,7 @@ import javax.swing.JOptionPane;
 
 public class GrafoRepresentacao {
 	
-	private HashMap<Vertice, ArrayList<Aresta>> mapa_adj;
+	private HashMap<Vertice, ArrayList<Vertice>> mapa_adj;
 	private ArrayList<Aresta> listaAresta;
 	private ArrayList<Vertice> listaVertice;
 	private int matriz_adj[][];
@@ -24,7 +24,7 @@ public class GrafoRepresentacao {
 	
 	public GrafoRepresentacao() {
 		
-		this.mapa_adj = new HashMap<Vertice, ArrayList<Aresta>>();
+		this.mapa_adj = new HashMap<Vertice, ArrayList<Vertice>>();
 		this.listaAresta = new ArrayList<Aresta>();
 		this.listaVertice = new ArrayList<Vertice>();
 		this.matriz_adj = new int [10][10];
@@ -38,7 +38,7 @@ public class GrafoRepresentacao {
 	public void direcao() {
 		
 		int resp = JOptionPane.showConfirmDialog(null,
-				"O grafo é direcionado?", "Grafos", JOptionPane.YES_NO_OPTION);
+				"O grafo e direcionado?", "Grafos", JOptionPane.YES_NO_OPTION);
 		
 		if (resp == 0)
 			ehDirecioanado = true; 
@@ -50,12 +50,12 @@ public class GrafoRepresentacao {
 		int rotulo = 0;
 		int num_vertices = 0;
 		
-		num_vertices = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o número de vértices do grafo:", 
+		num_vertices = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o numero de vertices do grafo:", 
                 "Grafos", JOptionPane.INFORMATION_MESSAGE));
 		
 		for (int i = 1; i <= num_vertices; i++) {
 			
-			rotulo = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o rotulo do "+i+"° vértice:", 
+			rotulo = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o rotulo do vertice "+i+":", 
 	                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 			Vertice v = new Vertice(rotulo);
 			
@@ -72,35 +72,41 @@ public class GrafoRepresentacao {
 			int rotulo_fim = 0;
 			int num_arestas = 0;
 			int peso = 0;
+			int cont = 1;
 			
-			num_arestas = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o número de arestas do grafo:", 
+			num_arestas = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o numero de arestas do grafo:", 
 	                "Grafos", JOptionPane.INFORMATION_MESSAGE));
+			
+			while(num_arestas <= 0) {
+				num_arestas = Integer.parseInt(JOptionPane.showInputDialog(null, "Valor invÃ¡lido.\nInforme novamente numero de arestas do grafo:", 
+		                "Grafos", JOptionPane.INFORMATION_MESSAGE));
+			}
 			
 			while(num_arestas > 0) {
 				
-				rotulo_origem = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o rotulo do vértice origem:", 
+				rotulo_origem = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o vertice origem da aresta "+cont+" :", 
 		                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 				
 				Vertice v1 = encontraVertice(rotulo_origem);
 				
 				while(v1 == null) {
-					rotulo_origem = Integer.parseInt(JOptionPane.showInputDialog(null, "Vértice não exitse.\nInforme o rotulo do vértice novamente:", 
+					rotulo_origem = Integer.parseInt(JOptionPane.showInputDialog(null, "Vertice nao exitse.\nInforme o rotulo do vertice origem da aresta novamente:", 
 			                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 					v1 = encontraVertice(rotulo_origem);
 				}
 				
-				rotulo_fim = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o rotulo do vértice origem:", 
+				rotulo_fim = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o vertice destino da aresta "+cont+" :", 
 		                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 				
 				Vertice v2 = encontraVertice(rotulo_fim);
 				
 				while(v2 == null) {
-					rotulo_fim = Integer.parseInt(JOptionPane.showInputDialog(null, "Vértice não exitse.\nInforme o rotulo do vértice novamente:", 
+					rotulo_fim = Integer.parseInt(JOptionPane.showInputDialog(null, "Vertice nao existe.\nInforme o rotulo do vertice destino da aresta novamente:", 
 			                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 					v2 = encontraVertice(rotulo_fim);
 				}
 				
-				peso = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o peso da aresta:", 
+				peso = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o peso da aresta "+cont+" :", 
 		                "Grafos", JOptionPane.INFORMATION_MESSAGE));
 				
 				Aresta a = new Aresta(v1, v2, peso);
@@ -108,8 +114,9 @@ public class GrafoRepresentacao {
 				listaAresta.add(a);
 				
 				num_arestas--;
-			}
-						
+				
+				cont++;
+			}						
 			
 		}
 		else {
@@ -134,16 +141,19 @@ public class GrafoRepresentacao {
 		
 		criaListaAdj();
 		
+		System.out.println("   ");
+		
 		for(Vertice v: mapa_adj.keySet()) {
+			
 			System.out.print(v.getRotulo()+" => ");
-			for(Aresta a: mapa_adj.get(v)) {
-				System.out.print(a.getVerticeDestino());
-				System.out.println(" => ");
+			
+			for(Vertice vertice: mapa_adj.get(v)) {
+				System.out.print(vertice.getRotulo()+" => ");				
 			}
+			System.out.print("o");
+			
 			System.out.println("");
 		}
-
-
 	}
 	
 	public void mostraMatrizAdj() {
@@ -152,8 +162,10 @@ public class GrafoRepresentacao {
 		
 		System.out.println("   ");
 		
+		System.out.print("****");
+		
 		for(Vertice v: listaVertice) {
-			System.out.print(v.getRotulo()+"   ");
+			System.out.print(v.getRotulo()+"    ");
 		}
 		
 		System.out.println();
@@ -172,7 +184,7 @@ public class GrafoRepresentacao {
 	private void criaListaAdj() {
 		
 		for(Vertice v: listaVertice) {
-			ArrayList<Aresta> arestasAdj = retornaListaArestaAdj(v);
+			ArrayList<Vertice> arestasAdj = listaAdj(v.getRotulo());
 			
 			mapa_adj.put(v, arestasAdj);
 		}
@@ -184,19 +196,19 @@ public class GrafoRepresentacao {
 			matriz_adj[a.getVerticeOrigem().getRotulo()][a.getVerticeDestino().getRotulo()] = 1;
 		
 	}
-	
-	private ArrayList<Aresta> retornaListaArestaAdj(Vertice v){
 		
-		ArrayList<Aresta> listaArestaAdj = new ArrayList<Aresta>();
+	public ArrayList<Vertice> listaAdj(int vertice){
+		
+		ArrayList<Vertice> listaAdj = new ArrayList<Vertice>();
 		
 		for(Aresta a: listaAresta) {
 			
-			if(a.getVerticeOrigem() == v) {
-				listaArestaAdj.add(a);
+			if(a.getVerticeOrigem().getRotulo() == vertice) {
+				listaAdj.add(a.getVerticeDestino());
 			}
 		}
 		
-		return listaArestaAdj;
+		return listaAdj;		
 	}
-
+	
 }
