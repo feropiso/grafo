1fb2db3e31d2b4b8180d34d9001a7f3eda80628f
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2023-05-17 09:49:52 -0300

    Update GrafoApp.java

--- src/grafo/GrafoApp.java
+++ src/grafo/GrafoApp.java
@@ -1,16 +1,20 @@
 package grafo;
 
+import grafos.buscas.BuscaEmLargura;
+import grafos.buscas.BuscaEmProfundidade;
+
 public class GrafoApp {
 
 	public static void main(String[] args) {
-				
+		
 		GrafoRepresentacao rep = new GrafoRepresentacao();
 		
-		rep.direcao();
-		rep.recebeVertices();
-		rep.criaArestas();
+		
 		rep.mostraListaAdj();
 		rep.mostraMatrizAdj();
+		
+		BuscaEmProfundidade dfs = new BuscaEmProfundidade(rep.getGrafo(), 0);
+		BuscaEmLargura  bfs = new BuscaEmLargura(rep.getGrafo(), 0);
 	}
 
 }
